dc345a2cebfa7eb96697f7a35076a91e5dceabd7
NottsKnight <ian.knight.1990@gmail.com>
2021-12-08 17:31:45 +0000

    Theoretically complete part2

--- lib/day01.ex
+++ lib/day01.ex
@@ -4,13 +4,23 @@ defmodule Day01 do
     [{x, y} | pairs([y | xs])]
   end
 
-  defp pairs(_) do
-    []
-  end
+  defp pairs(_), do: []
 
   @spec countIncreases([integer()]) :: integer()
   defp countIncreases(xs) do
     pairs(xs) |> Enum.map(fn {x, y} -> y > x end) |> Enum.count(fn x -> x end)
+  end
+
+  @spec triples([integer()]) :: [{integer(), integer(), integer()}]
+  defp triples([x | [y | [z | xs]]]) do
+    [{x, y, z} | triples([y | [z | xs]])]
+  end
+
+  defp triples(_), do: []
+
+  @spec countWindowIncreases([integer()]) :: integer()
+  defp countWindowIncreases(xs) do
+    triples(xs) |> Enum.map(fn {x, y, z} -> x + y + z end) |> countIncreases()
   end
 
   @spec main :: :ok
@@ -19,7 +29,7 @@ defmodule Day01 do
       {:ok, content} ->
         String.split(content)
         |> Enum.map(fn x -> Integer.parse(x) end)
-        |> countIncreases()
+        |> countWindowIncreases()
         |> IO.puts()
 
       {:error, reason} ->
