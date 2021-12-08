0ccfe82591d166337400e84736a054b30cea1fb5
NottsKnight <ian.knight.1990@gmail.com>
2021-12-08 20:10:59 +0000

    Complete day02

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,25 +1,33 @@
 defmodule Day02 do
-  @typep location :: {integer(), integer()}
+  # @typep location :: {integer(), integer()}
+  @typep locationAim :: {integer(), integer(), integer()}
   @typep command :: {String.t(), integer()}
 
-  @spec move(location(), command()) :: location()
-  defp move({pos, depth}, {direction, distance}) do
-    case direction do
-      "forward" -> {pos + distance, depth}
-      "up" -> {pos, depth + distance}
-      "down" -> {pos, depth - distance}
+  @spec move(command(), locationAim()) :: locationAim()
+  defp move({dir, dist}, {pos, depth, aim}) do
+    case dir do
+      "forward" -> {pos + dist, depth + (aim * dist), aim}
+      "up" -> {pos, depth, aim - dist}
+      "down" -> {pos, depth, aim + dist}
     end
   end
 
-  @spec makeMoves([command()], location()) :: location()
+  @spec makeMoves([command()], locationAim()) :: locationAim()
   defp makeMoves([cmd | cmds], loc) do
-    makeMoves(cmds, move(loc, cmd))
+    makeMoves(cmds, move(cmd, loc))
   end
 
   defp makeMoves([], loc), do: loc
 
-  @spec locMultiply(location()) :: integer()
-  defp locMultiply({pos, depth}) do
+  @spec parseCommand(String.t()) :: command()
+  defp parseCommand(cmd) do
+    [dir | [dist | _]] = String.split(cmd)
+    {dist1, _} = Integer.parse(dist)
+    {dir, dist1}
+  end
+
+  @spec locHash(locationAim()) :: integer()
+  defp locHash({pos, depth, _}) do
     pos * depth
   end
 
@@ -28,15 +36,13 @@ defmodule Day02 do
     case File.read("res/day02.dat") do
       {:ok, content} ->
         String.split(content, ~r/\r|\n|\r\n|\n\r/)
-        |> Enum.map(fn x -> String.split(x) end)
-        |> Enum.map(fn [cmd | [dist | []]] -> {cmd, Integer.parse(dist)} end)
-        |> Enum.map(fn {cmd, {dist, _}} -> {cmd, dist} end)
-        |> makeMoves({0, 0})
-        |> locMultiply()
+        |> Enum.map(fn s -> parseCommand(s) end)
+        |> makeMoves({0, 0, 0})
+        |> locHash()
         |> IO.puts()
 
       {:error, reason} ->
-        IO.puts(:stderr, "Failed to read: #{reason}")
+        IO.puts("Failed to read: #{reason}")
     end
   end
 end
