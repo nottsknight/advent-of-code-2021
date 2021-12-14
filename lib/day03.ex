341f896b4cba946bfc3f1f3cb8b9d237130d262e
NottsKnight <ian.knight.1990@gmail.com>
2021-12-14 16:34:09 +0000

    Finish day03 p1

--- lib/day03.ex
+++ lib/day03.ex
@@ -1,43 +1,49 @@
 defmodule Day03 do
-  use Bitwise, only_operators: true
-
   @spec parse(String.t()) :: [integer()]
   defp parse(str) do
     String.graphemes(str) |> Enum.map(&String.to_integer/1)
   end
 
-  @spec addTwoLists([integer()], [integer()]) :: [integer()]
-  defp addTwoLists([x | xs], [y | ys]) do
-    [x + y | addTwoLists(xs, ys)]
+  @spec add_lists([integer()], [integer()]) :: [integer()]
+  defp add_lists([x | xs], [y | ys]) do
+    [x + y | add_lists(xs, ys)]
   end
 
-  defp addTwoLists([], _), do: []
+  defp add_lists([], _), do: []
+  defp add_lists(_, []), do: []
 
-  defp addTwoLists(_, []), do: []
-
-  @spec calcRates([integer()]) :: {integer(), integer()}
-  defp calcRates(xs) do
-    threshold = Enum.count(xs) / 2
-
-    gamma =
-      Enum.map(xs, fn b -> if b >= threshold, do: 1, else: 0 end)
-      |> Enum.reduce(fn x, y -> (y <<< 1) + x end)
-
-    {gamma, ~~~gamma}
+  @spec gamma([integer()]) :: [integer()]
+  defp gamma(xs) do
+    n = Enum.count(xs) / 2
+    Enum.reduce(xs, &add_lists/2) |> Enum.map(fn x -> if x > n, do: 1, else: 0 end)
   end
 
-  @spec main :: :ok
+  @spec epsilon_from_gamma([integer()]) :: [integer()]
+  defp epsilon_from_gamma(bs) do
+    Enum.map(bs, fn b -> if b == 1, do: 0, else: 1 end)
+  end
+
+  @spec bits_to_integer([integer()]) :: integer()
+  defp bits_to_integer(bs) do
+    Enum.reduce(bs, fn b, acc -> (acc * 2) + b end)
+  end
+
+  @spec calc_values([integer()]) :: {integer(), integer()}
+  defp calc_values(xs) do
+    gamma_bits = gamma(xs)
+    epsilon_bits = epsilon_from_gamma(gamma_bits)
+    {bits_to_integer(gamma_bits), bits_to_integer(epsilon_bits)}
+  end
+
+  defp calc_result({gamma, epsilon}) do
+    gamma * epsilon
+  end
+
+  @spec main :: :ok | [integer]
   def main do
     case File.read("res/day03.dat") do
-      {:ok, content} ->
-        String.split(content)
-        |> Enum.map(&parse/1)
-        |> Enum.reduce(&addTwoLists/2)
-        |> calcRates()
-        |> IO.puts()
-
-      {:error, reason} ->
-        IO.puts("Failed to read: #{reason}")
+      {:error, reason} -> IO.puts(:stderr, "Failed to read: #{reason}")
+      {:ok, content} -> String.split(content) |> Enum.map(&parse/1) |> calc_values() |> calc_result() |> IO.puts()
     end
   end
 end
