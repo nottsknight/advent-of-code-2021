3a4b4b59c0c5528d1b10e31b94d648f042f5dd1f
NottsKnight <ian.knight.1990@gmail.com>
2021-12-15 11:34:59 +0000

    Start on day03 p2

--- lib/day03.ex
+++ lib/day03.ex
@@ -15,7 +15,7 @@ defmodule Day03 do
   @spec gamma([integer()]) :: [integer()]
   defp gamma(xs) do
     n = Enum.count(xs) / 2
-    Enum.reduce(xs, &add_lists/2) |> Enum.map(fn x -> if x > n, do: 1, else: 0 end)
+    Enum.reduce(xs, &add_lists/2) |> Enum.map(fn x -> if x >= n, do: 1, else: 0 end)
   end
 
   @spec epsilon_from_gamma([integer()]) :: [integer()]
@@ -39,14 +39,46 @@ defmodule Day03 do
     gamma * epsilon
   end
 
-  @spec main :: :ok | [integer]
+  @spec bit_filter([integer()], integer(), integer()) :: boolean()
+  defp bit_filter([b | _], mask, 0) do
+    b == mask
+  end
+
+  defp bit_filter([_ | bs], mask, idx) do
+    bit_filter(bs, mask, idx - 1)
+  end
+
+  @spec get_rating([[integer()]], [integer()], integer()) :: integer()
+  defp get_rating([x], _, _) do
+    bits_to_integer(x)
+  end
+
+  defp get_rating(values, [m | mask], idx) do
+    new_values = Enum.filter(values, fn val -> bit_filter(val, m, idx) end)
+    get_rating(new_values, mask, idx + 1)
+  end
+
+  @spec find_ratings([[integer()]]) :: {integer(), integer()}
+  defp find_ratings(values) do
+    o2_filter = gamma(values)
+    o2_rating = get_rating(values, o2_filter, 0)
+    co2_filter = epsilon_from_gamma(o2_filter)
+    co2_rating = get_rating(values, co2_filter, 0)
+    {o2_rating, co2_rating}
+  end
+
+  @spec main :: :ok
   def main do
     case File.read("res/day03.dat") do
       {:error, reason} ->
         IO.puts(:stderr, "Failed to read: #{reason}")
 
       {:ok, content} ->
-        String.split(content) |> Enum.map(&parse/1) |> calc_values() |> calc_result() |> IO.puts()
+        String.split(content)
+        |> Enum.map(&parse/1)
+        |> find_ratings()
+        |> calc_result()
+        |> IO.puts()
     end
   end
 end
